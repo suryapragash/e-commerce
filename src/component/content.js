7e4dd1af5f6444eae81cbfa412681a66578595e8
surya pragash <suryapragash.doodleblue@gmail.com>
2022-12-14 13:21:47 +0530

    hi

---
+++ src/component/content.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Content() {
+  return (
+    <div className="container">
+      <div className=" col-6 justify-content-start py-3">
+        <h1 className="text-start py-3">shopping Site</h1>
+        <h3 className="text-start py-3">
+          Build an app to power Shopify’s millions of merchants
+        </h3>
+        <p className="text-start">
+          Join the Shopify Partner Program for free and access educational
+          resources, developer preview environments, and recurring revenue share
+          opportunities.
+        </p>
+      </div>
+      <div className="col-6"></div>
+    </div>
+  );
+}
+export default Content;
