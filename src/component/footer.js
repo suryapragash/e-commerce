e80514125c954584b799ff5910916d5e6372c7d5
surya pragash <suryapragash.doodleblue@gmail.com>
2022-12-14 13:52:25 +0530

    hi

---
+++ src/component/footer.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <section className="bg-dark ">
+      <div className="" id="demo">
+        <div className="container p-3 ">
+          <div className="row">
+            <div className="col-3 text-start text-white">
+              <h1 className="py-3">onii</h1>
+              <p>123 Londan bridge street</p>
+              <p>london</p>
+              <p className="text-secondary">suppot@oni.com</p>
+            </div>
+            <div className="col-2 text-start text-white">
+              <h5 className="py-3">COMPANY</h5>
+              <p className="">our Store</p>
+              <p>About Us</p>
+              <p>Work With us</p>
+              <p>our Blog</p>
+            </div>
+            <div className="col-2 text-start text-white">
+              <h5 className="py-3">USEFULL LINK</h5>
+              <p>Help</p>
+              <p>Track My Otder</p>
+              <p>Men</p>
+              <p> Women</p>
+            </div>
+            <div className="col-2 text-start text-white">
+              <h5 className="py-3">SHOP GUID</h5>
+              <p>Shipping</p>
+              <p> Size Guide</p>
+              <p> Return</p>
+              <p>Size Guide</p>
+            </div>
+            <div className="col-3 text-start text-white">
+              <h5 className="py-3">DONT MISS ANYTHING</h5>
+              <p>sign up for email updates on the colmkmfn lekm jfkdmsdfs</p>
+              <input
+                type="text"
+                className="bg-dark"
+                placeholder="Email Address"
+              ></input>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Footer;
